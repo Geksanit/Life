f4f7f0cd82aaac2257f9f812f99de73e39ebeebd
Geksanit <31949379+Geksanit@users.noreply.github.com>
2017-12-22 18:46:48 +0700

    Тесты модели приведены в порядок.

--- frontend/mvc/model/Board.spec.js
+++ frontend/mvc/model/Board.spec.js
@@ -1,70 +1,91 @@
-
+/* global assert */
 import Board from './Board';
 
-describe('модель', function () {
-  describe('конструктор', function () {
-    const arr = new Board(2, 2);
-    it('матрица', function () {
-      const a = new Board(0, 0);
-      assert.deepEqual(a.matrix, [], 'not empty array');
-      assert.deepEqual(arr.matrix, [[false, false], [false, false]], 'должна быть матрица 2 на 2 с ложными значениями');
-    });
-    it('атрибуты', function () {
-      assert.equal(arr.m, 2);
-      assert.equal(arr.n, 2);
-    })
+describe('модель', () => {
+  const modelRows = 5;
+  const modelColumns = 5;
+  let model;
+  beforeEach(() => {
+    model = new Board(modelRows, modelColumns);
   });
-  describe('методы модели', function () {
-    let arr = new Board(2, 2);
-    it('set cell', function () {
-      arr.setCell(0, 0);
-      assert.equal(arr.matrix[0][0], true, 'переключение ячейки');
-      arr.setCell(0, 0);
-      assert.equal(arr.matrix[0][0], false, 'переключение ячейки');
+  describe('конструктор', () => {
+    it('матрица', () => {
+      const board = new Board(2, 2);
+      assert.deepEqual(board.matrix, [[false, false], [false, false]], 'должна быть матрица 2 на 2 с ложными значениями');
     });
-    it('clear', function () {
-      arr.setCell(0, 0);
-      arr.setCell(1, 1);
-      arr.clear();
-      assert.equal(arr.matrix[0][0], false, 'ячейки обнулились');
-      assert.equal(arr.matrix[1][1], false, 'ячейки обнулились');
+    it('атрибуты', () => {
+      assert.equal(model.rows, modelRows);
+      assert.equal(model.columns, modelColumns);
     });
-    arr = new Board(3, 3);
-    it('cell', function () {
-      arr.setCell(0, 0);
-      arr.setCell(0, 1);
-      assert.equal(arr.cell(1, 1), false, '2 живых соседа');
-
-      arr.setCell(1, 0);
-      assert.equal(arr.cell(1, 1), true, '3 живых соседа');
-
-      arr.setCell(0, 2);
-      assert.equal(arr.cell(1, 1), false, '4 живых соседа');
-    });
-    it('worker', function () {
-      arr.worker();
-      assert.deepEqual(arr.matrix, [[true, true, false], [true, false, false], [false, false, false]],'одна итерация');
-      const board = new Board(3, 3);
-      const oldBoard = board;
-      board.worker();
-      assert.equal(board, oldBoard, 'если матрица не меняется, ссылка остаетя актуальной');
-    });
-    describe('resize', function () {
-      it('увеличение', function () {
-        arr.resize(50, 40);
-        assert.equal(arr.matrix.length, 50, '50 строк');
-        assert.equal(arr.m, 50, '50 строк');
-        assert.equal(arr.matrix[0].length, 40, '40 столбцов');
-        assert.equal(arr.n, 40, '40 столбцов');
-        assert.equal(arr.matrix[49][39], false, 'значение');
+  });
+  describe('методы', () => {
+    describe('resizeMatrix', () => {
+      it('увеличение', () => {
+        model.resizeMatrix(50, 40);
+        assert.equal(model.matrix.length, 50, '50 строк матрицы');
+        assert.equal(model.rows, 50, '50 строк');
+        assert.equal(model.matrix[0].length, 40, '40 столбцов матрицы');
+        assert.equal(model.columns, 40, '40 столбцов');
+        assert.equal(model.matrix[49][39], false, 'значение ячейки');
       });
-      it('уменьшение', function () {
-        arr.resize(10, 9);
-        assert.equal(arr.matrix.length, 10, 'уменьшение');
-        assert.equal(arr.m, 10, '10 строк');
-        assert.equal(arr.matrix[0].length, 9, '9 столбцов');
-        assert.equal(arr.n, 9, '9 столбцов');
+      it('уменьшение', () => {
+        model.resizeMatrix(50, 40);
+        model.resizeMatrix(10, 9);
+        assert.equal(model.matrix.length, 10, '10 строк матрицы');
+        assert.equal(model.rows, 10, '10 строк');
+        assert.equal(model.matrix[0].length, 9, '9 столбцов матрицы');
+        assert.equal(model.columns, 9, '9 столбцов');
       });
+    });
+    it('clearMatrix', () => {
+      model.toggleCell(0, 0);
+      model.toggleCell(1, 1);
+      model.clearMatrix();
+      assert.equal(model.matrix[0][0], false, 'ячейки обнулились');
+      assert.equal(model.matrix[1][1], false, 'ячейки обнулились');
+    });
+    it('calculateMatrix', () => {
+      const arr = new Board(3, 3);
+      let flag;
+      arr.toggleCell(0, 0); arr.toggleCell(0, 1); arr.toggleCell(1, 0);
+      assert.deepEqual(arr.matrix, [
+        [true, true, false], [true, false, false], [false, false, false],
+      ]);
+
+      flag = arr.calculateMatrix();
+      assert.deepEqual(arr.matrix, [
+        [true, true, false], [true, true, false], [false, false, false],
+      ]);
+      assert.equal(flag, false, 'матрица не повторилась');
+
+      flag = arr.calculateMatrix();
+      assert.deepEqual(arr.matrix, [
+        [true, true, false], [true, true, false], [false, false, false],
+      ]);
+      assert.equal(flag, true, 'матрица повторилась');
+    });
+    it('isRepeat', () => {
+      let value = model.isRepeatMatrix(model.matrix);
+      assert.equal(value, false, 'первая проверка');
+      value = model.isRepeatMatrix(model.matrix);
+      assert.equal(value, true, 'вторая проверка');
+    });
+    it('calculateCell', () => {
+      model.toggleCell(0, 0);
+      model.toggleCell(0, 1);
+      assert.equal(model.calculateCell(1, 1), false, '2 живых соседа');
+
+      model.toggleCell(1, 0);
+      assert.equal(model.calculateCell(1, 1), true, '3 живых соседа');
+
+      model.toggleCell(0, 2);
+      assert.equal(model.calculateCell(1, 1), false, '4 живых соседа');
+    });
+    it('toggleCell', () => {
+      model.toggleCell(0, 0);
+      assert.equal(model.matrix[0][0], true, 'переключение ячейки');
+      model.toggleCell(0, 0);
+      assert.equal(model.matrix[0][0], false, 'переключение ячейки');
     });
   });
 });
